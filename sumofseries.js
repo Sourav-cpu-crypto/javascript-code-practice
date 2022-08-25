5c6bd97ec14b066f9a5469ddec7cc67db0909c59
Sourav-cpu-crypto <66194084+Sourav-cpu-crypto@users.noreply.github.com>
2022-08-25 20:36:35 +0530

    Add files via upload

---
+++ sumofseries.js
@@ -0,0 +1,21 @@
+function sumOfSeries(N, X) {
+      sum=0;
+     p=1;
+     if(N <=0 || X <=0)
+     {
+         console.log("-1");
+     }
+    
+     else if(N >=1 && N <=20 && X >=1 
+     && X <=10 )
+     {
+   for (i=0;i<N;i++)
+   {
+     
+         sum=sum+X**(i);
+       
+   }
+     console.log(sum );
+     } 
+  
+}
